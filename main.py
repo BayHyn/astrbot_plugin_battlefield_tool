c235959f2deef4ad771c957ef9831299c3188af5
SHOOTING_STAR_C <944153243@qq.com>
2025-10-05 22:07:15 +0800

    refactor:增加可读性

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import aiohttp
     "astrbot_plugin_battlefield_tool",  # name
     "SHOOTING_STAR_C",  # author
     "战地风云战绩查询插件",  # desc
-    "v1.9.0",  # version
+    "v1.9.1",  # version
 )
 class BattlefieldTool(Star):
 
