54470887f0dc4caf4f96512cb8d7a9a5f3111fc1
SHOOTING_STAR_C <944153243@qq.com>
2025-10-05 21:59:46 +0800

    style:请求参数类

---
+++ models/player_data.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+from typing import Union
+
+@dataclass
+class PlayerDataRequest:
+    message_str: str
+    lang: str
+    qq_id: str
+    ea_name: Union[str, None]
+    game: Union[str, None]
+    server_name: Union[str, None]
+    error_msg: Union[str, None]
